d282a57c9bc4faded4ec4cac6b285520ea9c29f8
oujunxu <oujun96@gmail.com>
2021-08-28 13:29:52 +0200

    Added dataannotations for validation in character creation page. Added Regular expressions. Changed CSS and HTML for CreateCharacter.

--- DataLibrary/Models/CharacterModel.cs
+++ DataLibrary/Models/CharacterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DataLibrary.Models
@@ -7,12 +8,35 @@ namespace DataLibrary.Models
     public class CharacterModel
     {
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 3)] 
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+
+        [StringLength(50, MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")]
+        [Required(ErrorMessage = "Must contain a number and characters.")]
         public string Rarity { get; set; }
+
+        [RegularExpression(@"^(A-Za-z)[-/./␣]([0-31])$")]
+        [StringLength(50)]
+        [Required(ErrorMessage = "Must contain month and date.")]
         public string Birthday { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [Required(ErrorMessage = "Allegiance/Alliance is required.")]
         public string Allegiance { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [Required(ErrorMessage = "Must contain: Cryo/Pyro/Anemo/Electro/Geo/Hydro.")]
         public string Element { get; set; }
+
+        [StringLength(1024, MinimumLength = 10)]
+        [Required(ErrorMessage = "Image is required.")]
         public string Image { get; set; }
+
+        [StringLength(1024, MinimumLength = 50)]
+        [Required(ErrorMessage = "Description is required.")]
         public string Description { get; set; }
 
     }
